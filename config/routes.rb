e88afc9b97988e3bb807ac348209f62d68fb9dd6
Konstantinos Mavroudakis <kmavroudakis@gmail.com>
2013-10-08 14:53:00 +0300

    blog v1.02

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Blog::Application.routes.draw do
-  get "welcome/index"
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  resources :posts
+  # get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root to:'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
