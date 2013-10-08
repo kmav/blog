7a299bb36a40bc092d7a3b5e00351bb824d507ec
Konstantinos Mavroudakis <kmavroudakis@gmail.com>
2013-10-08 17:57:40 +0300

    blog v1.12

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,15 @@ class PostsController < ApplicationController
   end
   
   def create
-    render text: params[:post].inspect
+    @post = Post.new(post_params)
+ 
+    @post.save
+    redirect_to @post
+    #  render text: params[:post].inspect
+  end
+ 
+private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
